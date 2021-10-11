5990d617ee291fd7d672da0205212ac240dddc97
iggyi <iccyn@pm.me>
2021-10-11 18:30:04 +1100

    added code

--- Crowdsale.sol
+++ Crowdsale.sol
@@ -8,15 +8,25 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/distribution/RefundablePostDeliveryCrowdsale.sol";
 
 // @TODO: Inherit the crowdsale contracts
-contract PupperCoinSale is {
-
+contract PupperCoinSale is Crowdsale, MintedCrowdsale, CappedCrowdsale, TimedCrowdsale, RefundablePostDeliveryCrowdsale {
     constructor(
         // @TODO: Fill in the constructor parameters!
+        uint rate,
+        address payable wallet,
+        PupperCoin token, 
+        uint goal, 
+        uint open, 
+        uint close
     )
+
         // @TODO: Pass the constructor parameters to the crowdsale contracts.
+        Crowdsale(rate, wallet, token)
+        TimedCrowdsale(open, close)
+        CappedCrowdsale(goal)
+        RefundableCrowdsale(goal)
         public
     {
-        // constructor can stay empty
+    // constructor can stay empty
     }
 }
 
@@ -27,12 +37,19 @@ contract PupperCoinSaleDeployer {
 
     constructor(
         // @TODO: Fill in the constructor parameters!
+        string memory name, 
+        string memory symbol,
+        address payable wallet
     )
         public
     {
         // @TODO: create the PupperCoin and keep its address handy
-
+            PupperCoin token = new PupperCoin(name, symbol, 0);
+            token_address = address(token);
+    
         // @TODO: create the PupperCoinSale and tell it about the token, set the goal, and set the open and close times to now and now + 24 weeks.
+            PupperCoinSale token_sale = new PupperCoinSale(1, wallet, token, 300000000000000000000, now, now + 24 weeks);
+            token_sale_address = address(token_sale);
 
         // make the PupperCoinSale contract a minter, then have the PupperCoinSaleDeployer renounce its minter role
         token.addMinter(token_sale_address);
